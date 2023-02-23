18e058593e74c47f89565e89a50e0cf112957ec2
Mariano Latrecchiana <Mariano.latrecchiana@hotmail.com>
2023-02-22 22:34:04 -0300

    carrito de compra pizza

---
+++ js/products.js
@@ -0,0 +1,44 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "Muzzarella y Jamón",
+    precio: 1000,
+    img: (src = "../img/muzzarellayjamon.jpg"),
+    cantidad: 1,
+  },
+  {
+    id: 2,
+    nombre: "Muzzarella y Panceta",
+    precio: 1200,
+    img: (src = "../img/muzzarellaypanceta.jpg"),
+    cantidad: 1,
+  },
+  {
+    id: 3,
+    nombre: "Rucula y Jamón",
+    precio: 1500,
+    img: (src = "../img/ruculayjamon.jpg"),
+    cantidad: 1,
+  },
+  {
+    id: 4,
+    nombre: "Tomate y Albahaca",
+    precio: 1700,
+    img: (src = "../img/tomateyalbahaca.jpg"),
+    cantidad: 1,
+  },
+  {
+    id: 4,
+    nombre: "Fugazzetta Rellena",
+    precio: 2000,
+    img: (src = "../img/fugazzettarellena.jpg"),
+    cantidad: 1,
+  },
+  {
+    id: 4,
+    nombre: "Fugazzeta y Pepperoni",
+    precio: 2200,
+    img: (src = "../img/fugazzettaypepperoni.jpg"),
+    cantidad: 1,
+  },
+];
